a2c8966a1942750fd55d1979b6da16d44b3cbe50
ojlennon <45679125+ojlennon@users.noreply.github.com>
2024-03-12 13:56:35 -0600

    Changed new InstantCommand to runOnce

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "main2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 64;
-  public static final String GIT_SHA = "2180d4c47cfca750108dbd02f2eabef20c29449a";
-  public static final String GIT_DATE = "2024-03-12 09:07:22 MDT";
+  public static final int GIT_REVISION = 65;
+  public static final String GIT_SHA = "884652d484a823212e2684f3d99aef9c2d695448";
+  public static final String GIT_DATE = "2024-03-12 09:47:39 MDT";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2024-03-12 09:47:18 MDT";
-  public static final long BUILD_UNIX_TIME = 1710258438631L;
+  public static final String BUILD_DATE = "2024-03-12 13:53:35 MDT";
+  public static final long BUILD_UNIX_TIME = 1710273215738L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
